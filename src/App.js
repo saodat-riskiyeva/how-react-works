a08328238343c2b8ca14cb63b4df34540702b7b7
Saodat <saodat.riskiyeva@gmail.com>
2025-05-10 22:10:03 -0400

    state update with callback function

--- src/App.js
+++ src/App.js
@@ -71,10 +71,19 @@ function TabContent({ item }) {
     setLikes(likes + 1);
   }
 
+  function handleTripleInc() {
+    // setLikes(likes + 1);
+    // setLikes(likes + 1);
+    // setLikes(likes + 1);
+
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+  }
+
   function handleUndo() {
     setShowDetails(true);
     setLikes(0);
-    console.log(likes);
   }
 
   return (
@@ -90,7 +99,7 @@ function TabContent({ item }) {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTripleInc}>+++</button>
         </div>
       </div>
 
