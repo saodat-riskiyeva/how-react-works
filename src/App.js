a3c0136a46e850e633703b00d89e15db104b51d6
Saodat <saodat.riskiyeva@gmail.com>
2025-05-10 22:03:11 -0400

    state update batching

--- src/App.js
+++ src/App.js
@@ -65,8 +65,16 @@ function TabContent({ item }) {
   const [showDetails, setShowDetails] = useState(true);
   const [likes, setLikes] = useState(0);
 
+  console.log("RENDER");
+
   function handleInc() {
     setLikes(likes + 1);
+  }
+
+  function handleUndo() {
+    setShowDetails(true);
+    setLikes(0);
+    console.log(likes);
   }
 
   return (
@@ -87,7 +95,7 @@ function TabContent({ item }) {
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
+        <button onClick={handleUndo}>Undo</button>
         <button>Undo in 2s</button>
       </div>
     </div>
